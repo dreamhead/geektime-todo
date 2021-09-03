d9f64d399665dcf1796e529bbf7c6c0b63c48a4e
dreamhead <dreamhead.cn@gmail.com>
2021-09-03 22:24:39 +0800

    applied test db to todo item resource test

--- todo-api/src/test/java/com/github/dreamhead/todo/api/TodoItemResourceTest.java
+++ todo-api/src/test/java/com/github/dreamhead/todo/api/TodoItemResourceTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@TestPropertySource("classpath:test.properties")
 public class TodoItemResourceTest {
     @Autowired
     private MockMvc mockMvc;
